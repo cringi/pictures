167214cc20f71a34b1ec6924cb7613e2b6fe61a5
Brandon Berney <brandonberney@protonmail.com>
2016-04-20 10:35:26 -0400

    code cleanup

--- src/FileChooser.java
+++ src/FileChooser.java
@@ -1,43 +1,42 @@
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import java.io.*;
-import java.net.*;
-  
+import javax.swing.*;
+import java.io.File;
+import java.net.URL;
+import java.net.URLDecoder;
+
 /**
  * A class to make working with a file chooser easier
  * for students.  It uses a JFileChooser to let the user
  * pick a file and returns the chosen file name.
- * 
+ *
  * @author Barb Ericson ericson@cc.gatech.edu
  */
-public class FileChooser 
-{
-  
+public class FileChooser {
+
   /////////////////////// methods /////////////////////////////
-  
+
   /**
    * Method to get the full path for the passed file name
+   *
    * @param fileName the name of a file
    * @return the full path for the file
    */
-  public static String getMediaPath(String fileName) 
-  {
+  public static String getMediaPath(String fileName) {
     String path = null;
     String directory = getMediaDirectory();
     boolean done = true;
-    
+
     // get the full path
     path = directory + fileName;
     return path;
   }
-  
+
   /**
    * Method to pick an item using the file chooser
+   *
    * @param fileChooser the file Chooser to use
    * @return the path name
    */
-  public static String pickPath(JFileChooser fileChooser)
-  {
+  public static String pickPath(JFileChooser fileChooser) {
     String path = null;
     
     /* create a JFrame to be the parent of the file 
@@ -46,29 +45,29 @@ public class FileChooser
      */
     JFrame frame = new JFrame();
     frame.setAlwaysOnTop(true);
-    
+
     // get the return value from choosing a file
     int returnVal = fileChooser.showOpenDialog(frame);
-    
-    // if the return value says the user picked a file 
+
+    // if the return value says the user picked a file
     if (returnVal == JFileChooser.APPROVE_OPTION)
       path = fileChooser.getSelectedFile().getPath();
     return path;
   }
-  
+
   /**
    * Method to let the user pick a file and return
-   * the full file name as a string.  If the user didn't 
+   * the full file name as a string.  If the user didn't
    * pick a file then the file name will be null.
+   *
    * @return the full file name of the picked file or null
    */
-  public static String pickAFile()
-  {
+  public static String pickAFile() {
     JFileChooser fileChooser = null;
-    
+
     // start off the file name as null
     String fileName = null;
-    
+
     // get the current media directory
     String mediaDir = getMediaDirectory();
     
@@ -81,45 +80,45 @@ public class FileChooser
         fileChooser = new JFileChooser(file);
     } catch (Exception ex) {
     }
-    
+
     // if no file chooser yet create one
     if (fileChooser == null)
       fileChooser = new JFileChooser();
-    
+
     // pick the file
     fileName = pickPath(fileChooser);
-    
+
     return fileName;
-  }
-  
+    }
+
   /**
    * Method to get the directory for the media
+   *
    * @return the media directory
    */
-  public static String getMediaDirectory() 
-  {
+  public static String getMediaDirectory() {
     String directory = null;
     boolean done = false;
     File dirFile = null;
-    
-    // try to find the images directory
-      try {
-        // get the URL for where we loaded this class 
-        Class currClass = Class.forName("FileChooser");
-        URL classURL = currClass.getResource("FileChooser.class");
-        URL fileURL = new URL(classURL, "images/");
-        directory = fileURL.getPath();
-        directory = URLDecoder.decode(directory, "UTF-8");
-        dirFile = new File(directory);
-        if (dirFile.exists()) {
-          //setMediaPath(directory);
-          return directory;
-        }
-      } catch (Exception ex) {
-      }
 
-      System.out.println(directory);
-      return directory;
+    // try to find the images directory
+    try {
+      // get the URL for where we loaded this class
+      Class currClass = Class.forName("FileChooser");
+      URL classURL = currClass.getResource("FileChooser.class");
+      URL fileURL = new URL(classURL, "images/");
+      directory = fileURL.getPath();
+      directory = URLDecoder.decode(directory, "UTF-8");
+      dirFile = new File(directory);
+      if (dirFile.exists()) {
+        //setMediaPath(directory);
+        return directory;
+      }
+    } catch (Exception ex) {
+        }
+
+    System.out.println(directory);
+    return directory;
   }
-  
+
 }
